c28466a320fc2f93eb2eb06f5c610b0416c994e6
katharine-atx <rovinsky.katharine@gmail.com>
2016-06-14 19:16:44 -0500

    MapReduce join example for viewership by network/show

---
+++ join1_reducer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# Task: Calculate total number of viewers by show for ABC given shows/station, show/viewers.
+# join1_reduce.py provides the reduce() function for Map/Reduce
+
+import sys
+
+# Initialize variables...
+prev_show = ""
+ABC_found = False
+total_views	= 0
+
+for entry in sys.stdin:
+	entry             = entry.strip()       #strip out carriage return
+	curr_show, value  = entry.split('\t')         
+	# Output of map() function will generate key/values with either "ABC" or viewer count for value...
+	if curr_show != prev_show:
+			if ABC_found:
+				print(prev_show," ",total_views)
+				# And then reset defaults...
+				total_views = 0
+				ABC_found = False
+	prev_show = curr_show
+	if value == "ABC":
+		ABC_found == True
+	else:
+		total_views += int(value_in)
+if ABC_found:
+	print(prev_show," ",total_views)
---
+++ wordcountjoin_trend.java
@@ -0,0 +1,62 @@
+# HDFS Word Count Trend Assignment with Map/Reduce - Joining data
+# Note: for Cloudera hadoop configuration accessed via VM
+
+# Task: Calculate total number of viewers by show for ABC given shows/station, show/viewers.
+
+# Data Notes:
+	# Channels labels have 3 letters
+	# TV show titles can appear multiple times, with different counts
+	# A TV show and channel combination might appear multiple times
+	# TV shows could appear on multiple channels
+	# The output should have no commas or punctuation, only 1 space between the TV show title and number
+	
+# ------------------------------
+
+# Initiating map() and reduce() python scripts plus txt inputs...
+gedit make_join2data.py
+gedit make_data_join2.txt
+gedit join1_mapper.py
+gedit join1_reducer.py
+
+# Note: add the script from the referenced .py/.txt files to these
+# new objects in the VM environment
+
+# Executing...
+chmod +x join1_mapper.py
+chmod +x join1_reducer.py
+chmod +x make_join2data.py
+
+# Get out of safe mode if necessary so you can make changes on the name node...
+hdfs dfsadmin -safemode leave
+
+# Debugging python scripts: Confirm that the mapper script is working ok outside 
+# map/reduce, and not using what is in the HDFS file system.
+# cat join1_File*.txt | ./join1_mapper.py | sort | ./join1_reducer.py
+# If these commands work you should see the all the <word, total count>'s , 
+# and then you can try running in hadoop. 
+
+# Generating some data inputs
+sh make_data_join2.txt
+
+# Copy the input files from local filesystem to the HDFS filesystem. 
+hdfs dfs -put /home/cloudera/join2_gen*.txt  /user/cloudera/input
+
+# Confirm files on HDFS...there should be a total of 6 output files
+hdfs dfs -ls /user/cloudera/input
+
+# Run the Hadoop streaming command for specified input/output...
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+-input /user/cloudera/input \
+-output /user/cloudera/output_join2 \
+-mapper /home/cloudera/"join1_mapper.py" \
+-reducer /home/cloudera/"join1_reducer.py" \
+-numReduceTasks 1
+
+# View the output directory...
+hdfs dfs -ls /user/cloudera/output_join2
+
+# Look at the files there and check out the contents...
+hdfs dfs -cat /user/cloudera/output_join2/part-00000
+
+# Save output file from join as total_viewer_counts.txt...
+hdfs dfs -getmerge /user/cloudera/output_join2/* total_viewer_counts.txt
